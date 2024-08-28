b5c2f1d500157e154b5e1279416fb3a347a6ebf2
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-08-28 11:38:21 +0200

    fixed pylint issues

--- car.py
+++ car.py
@@ -6,6 +6,9 @@ from typing import List
 
 @dataclass(frozen=True)
 class Car:
+    """
+    A simple Car class that contains the brand, mileage, and service dates.
+    """
     brand: str
     mileage: int
     service_dates: List[str] = field(default_factory=list)
